048b96c23eed5bdcae541338fd343d651e37c1cd
Requiem98 <amedeo.ranaldi@gmail.com>
2021-11-12 13:22:28 +0100

    aggiornato

--- functions.py
+++ functions.py
@@ -52,8 +52,8 @@ def save_html_AnimePage_In_ListAnimePage(urls, folderNumber, CPUs = multiprocess
 def get_listAnimePage(index, listPages):
     listPages[index] = requests.get(f"https://myanimelist.net/topanime.php{'?limit={}'.format(50*index)}")
     
-    """if(listPages[index].status_code != 200) : 
-        raise Exception(f"Web site have closed the connection at page: {index}")"""
+    if(listPages[index].status_code != 200) : 
+        raise Exception(f"Web site have closed the connection at page: {index}")
 
 
 
@@ -73,7 +73,7 @@ def get_urls_In_ListAnimePage(page, pages):
     pages[pages.index(page)] = animeLinks
     
     
-def initGet(pageToGet = 400 ,CPUs = multiprocessing.cpu_count()):
+def initGet(pageToGet = 383 ,CPUs = multiprocessing.cpu_count()):
 
     pages = [None] * pageToGet
     numberOfPage = range(0, pageToGet)
@@ -134,24 +134,46 @@ def findUsers(string):
 def str_to_datetime(d): #Convert a string into a datetime type
     """Input: string,
     Output: list"""
-    
-    d= re.sub(r',', '', d) #first of all, remove the comma from the string
-    #print(d)
-    if "to" in d:       
-        date_time_list = d.split(" to ") #split the date of start and the date of end
-        [start,end] = date_time_list[:]
+    if d=="Not available":
+        return None
+    else: 
+        d = re.sub(r',', '', d) #first of all, remove the comma from the string
+        d = re.sub(r' ','',d) #remove also the space
+       
+        if "to" in d:       
+            date_time_list = d.split("to") #split the date of start and the date of end
+            [start,end] = date_time_list[:]
+            
+            if len(start)==4: #if is only year
+                start_datetime = datetime.strptime(start, "%Y").date()
+            elif len(start)==7: #if is year and month
+                start_datetime = datetime.strptime(start, "%b%Y").date()
+            else:
+                start_datetime = datetime.strptime(start, "%b%d%Y").date()
+                
+            
+            if "?" in end:
+                end_datetime = None
+                return [start_datetime,end_datetime]
+            else:
+                
+                if len(end)==4: #if is only year
+                    end_datetime = datetime.strptime(end, "%Y").date()
+                elif len(end)==7: #if is year and month
+                    end_datetime = datetime.strptime(end, "%b%Y").date()
+                else:
+                    end_datetime = datetime.strptime(end, "%b%d%Y").date()
         
-        #convert start and end into datetime 
-        start_datetime = datetime. strptime(start, "%b %d %Y").date()
-        if end=="?":
-            end_datetime = None
-        else:   
-            end_datetime = datetime. strptime(end, "%b %d %Y").date()
+                return [start_datetime,end_datetime]
         
-        return [start_datetime,end_datetime]
-    else:
-        start_datetime = datetime. strptime(d, "%b %d %Y").date()
-        return[start_datetime, start_datetime]
+        else: #there is only the date of starting
+            if len(d)==4: #if is only year
+                start_datetime = datetime.strptime(d, "%Y").date()
+            elif len(d)==7: #if is year and month
+                start_datetime = datetime.strptime(d, "%b%Y").date()
+            else:
+                start_datetime = datetime.strptime(d, "%b%d%Y").date()
+            return[start_datetime,start_datetime]
 
 
 
@@ -196,7 +218,9 @@ def getDataFromPage(pagePath):
         finalDict["Episodes"] = None
 
     if(tempDict["Aired"] != "N/A" and tempDict["Aired"] != "Not available"):
-        finalDict["Aired"]  = tempDict["Aired"]#releasedDate and endDate
+        aired = str_to_datetime(tempDict["Aired"])
+        finalDict["releasedDate"]  = aired[0]
+        finalDict["endDate"] = aired[1]
     else:
         finalDict["Aired"] = None
 
@@ -541,7 +565,7 @@ def make_dataframe(animePath = anime_tsv_path()):
     
     with open("./generic_datafile/urls_anime.txt", "r") as file:
         lines = file.read().splitlines()
-        tsv_data['url'] = np.resize(lines[:-12],len(tsv_data))
+        tsv_data['url'] = np.resize(lines,len(tsv_data))
 
     tsv_data.to_csv("./generic_datafile/dataset.csv")
         
